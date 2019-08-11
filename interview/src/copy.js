5b86afa275fb3b99642d9cfeac932254aeeee004
ZhangDaZongWei <15516562272@163.com>
2019-08-11 18:17:27 +0800

    add regax

--- interview/src/copy.js
+++ interview/src/copy.js
@@ -40,3 +40,28 @@ console.log('deepClone: ', deepClone)
 obj.habit.push('me')
 
 console.log('deepClone: ', deepClone)
+
+// 上述使用JSON来处理，会存在很多问题
+// 因为JSON的有效值的类型 int string Boolean null object array 不能为function undefined 对象实例 变量
+// 所以在对象序列化JSON时，会忽略掉值为function undefined
+// 另外对于symbol类型也会忽略
+// 不能正确处理reg类型, 有的也说包括Date类型, 但是如下测试结果并没有问题
+// 无法处理循环引用, 会出现报错
+
+const i = 'hahaha'
+const timer = new Date()
+console.log('timer: ',timer)
+
+const jsonObj = {
+  name: 'zhangzongwei',
+  age: function() { return 25 },
+  address: undefined,
+  company: new Object(),
+  habit: i,
+  timer,
+  grade: Symbol('zhangzongwei'),
+  reg: /123/
+}
+
+console.log('jsonObj to Json: ', JSON.stringify(jsonObj))
+console.log('timerObj to Json: ', JSON.parse(JSON.stringify(timer)))
