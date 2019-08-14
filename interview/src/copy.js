8cae22caf483ac79732caf1037445b3075244313
ZhangDaZongWei <15516562272@163.com>
2019-08-15 07:30:04 +0800

    add symbiol

--- interview/src/copy.js
+++ interview/src/copy.js
@@ -66,7 +66,7 @@ const jsonObj = {
 console.log('jsonObj to Json: ', JSON.stringify(jsonObj))
 console.log('timerObj to Json: ', JSON.parse(JSON.stringify(timer)))
 
-// for in不能遍历到symbol类型的属性吗？
+// for in不能遍历到symbol类型的属性吗？不可以
 
 let age = Symbol('age')
 let address = Symbol('address')
@@ -74,8 +74,8 @@ let address = Symbol('address')
 
 const symbolObj = {
   name: 'zhangdazongwei',
-  age: 25,
-  address: 'xuchuang'
+  [age]: 25,
+  [address]: 'xuchuang'
 }
 
 for (let key in symbolObj) {
@@ -160,17 +160,32 @@ function isExist(arr,obj) {
 }
 
 // 3. 引用丢失的情况也是用第二种方式解决的
+// 4. 当对象中有Symbol值作为属性时，普通遍历方法没法获取其值，所以只能通过
+// Object.getOwnPropertySymbols(...)，返回一个数组，里面包含属性名的类型都是Symbol
+// Reflect.ownKeys(...)返回一个由目标对象自身的属性键组成的数组，当然包含Symbol
 
 function deepCopy(source,hash =[]) {
   if (!isObject(source)) return source
 
   if (isExist(hash,source)) return isExist(hash,source)
 
-  let target = Array.isArray(source) ? [] : {}
+  let target = Array.isArray(source) ? [] : {} 
   hash.push({
     source: source,
     target: target
   })
+
+  // 处理Symbol
+  let symbolProperty = Object.getOwnPropertySymbols(source)
+  if (symbolProperty.length) {
+    symbolProperty.forEach(item => {
+      if (isObject(source[item])) {
+        target[item] = deepCopy(source[item])
+      } else {
+        target[item] = source[item]
+      }
+    })
+  }
 
   for (let key in source) {
     if (isObject(source[key])) {
@@ -195,3 +210,23 @@ source1.reference = source1
 const deepCopy2 = deepCopy(source1)
 
 console.log('deepCopy2: ',deepCopy2)
+
+const habbit = Symbol('habbit')
+source1[habbit] = 'habbit'
+
+const deepCopy3 = deepCopy(source1)
+
+console.log('deepCopy3: ',deepCopy3)
+
+// 
+var obj1 = { foo: 'bar', x: 42 };
+var obj2 = { foo: 'baz', y: 13 };
+const merge = ( ...objects ) => {
+  console.log('object: ',objects)
+};
+
+var mergedObj = merge ( obj1, obj2);
+// { 0: { foo: 'bar', x: 42 }, 1: { foo: 'baz', y: 13 } }
+
+var mergedObj = merge ( {}, obj1, obj2);
+// { 0: {}, 1: { foo: 'bar', x: 42 }, 2: { foo: 'baz', y: 13 } }
