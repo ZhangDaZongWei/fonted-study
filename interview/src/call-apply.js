7b416219324474e272b130545aa657c19ade84fb
ZhangDaZongWei <15516562272@163.com>
2019-08-16 06:40:01 +0800

    add call/apply

---
+++ interview/src/call-apply.js
@@ -0,0 +1,36 @@
+/**
+ * 1. call和apply方法调用一个函数, 其具有一个指定的 this 值和分别地提供的参数(参数的列表)
+ */
+
+// 连接数组
+
+const array1 = ['a','b','c']
+const array2 = ['d','e','f']
+
+Array.prototype.push.apply(array1,array2)
+
+console.log('array1: ',array1)
+
+// 一个函数所能接受的参数个数是一定的，不同引擎的限制是不一样的，js核心限制在65535
+// 如下是解决方式，即将参数给拆开，然后多次push
+
+function concatArray(array1,array2) {
+  const spliceNum = 32768
+  for (let i=0, len=array2.length; i < len; i+=spliceNum) {
+    Array.prototype.push.apply(array1,array2.slice(i,Math.min(i+spliceNum,len)))
+  }
+  return array1
+}
+
+const array3 = ['zhang','zong','wei']
+const array4 = []
+for (let i=0; i<1000000; i++) {
+  array4.push(i)
+}
+
+// 出现堡栈的情况
+// Array.prototype.push.apply(array3,array4)
+
+const concatArray1 = concatArray(array3,array4)
+
+console.log('concatArray1: ',concatArray1)
