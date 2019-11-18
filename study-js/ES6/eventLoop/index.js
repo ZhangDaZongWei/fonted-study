eac6cfcca0cb88eaeed5507f63fc22ac49c7a2f5
ZhangDaZongWei <15516562272@163.com>
2019-11-18 21:33:10 +0800

    common

--- study-js/ES6/eventLoop/index.js
+++ study-js/ES6/eventLoop/index.js
@@ -1,8 +1,12 @@
+
 console.log('event loop start ...')
+
+let promise
 
 setTimeout(() => {
   console.log('setTimeout1')
   Promise.resolve('promise').then(res => {
+    promise = res
     console.log(res)
   })
 },10)
@@ -11,4 +15,4 @@ setTimeout(() => {
   console.log('setTimeout2')
 },1)
 
-console.log('event loop end ...')
+console.log('event loop end ...', promise)
